cdcea7224725f369cbb50788dd3356f80bf21002
aleupons <apahoquei@gmail.com>
2021-06-18 19:58:24 +0200

    Primera lletra en majúscula

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -5,14 +5,24 @@ export const Frase = (props) => {
   const { frase, borrarPalabra } = props;
   return (
     <ul className="resultado">
-      {frase.map((palabra) => (
-        <Palabra
-          key={palabra.id}
-          palabra={palabra}
-          borrarPalabra={borrarPalabra}
-          frase={true}
-        />
-      ))}
+      {frase.map((palabra, i) =>
+        i === 0 ? (
+          <Palabra
+            key={palabra.id}
+            palabra={palabra}
+            borrarPalabra={borrarPalabra}
+            frase={true}
+            first={true}
+          />
+        ) : (
+          <Palabra
+            key={palabra.id}
+            palabra={palabra}
+            borrarPalabra={borrarPalabra}
+            frase={true}
+          />
+        )
+      )}
     </ul>
   );
 };
--- src/components/Palabra.js
+++ src/components/Palabra.js
@@ -1,12 +1,14 @@
 import PropTypes from "prop-types";
 
 export const Palabra = (props) => {
-  const { palabra, anyadirPalabra, borrarPalabra, frase } = props;
+  const { palabra, anyadirPalabra, borrarPalabra, frase, first } = props;
   return (
     <li
       onClick={() => (frase ? borrarPalabra(palabra) : anyadirPalabra(palabra))}
     >
-      {palabra.palabra}
+      {first
+        ? palabra.palabra.charAt(0).toUpperCase() + palabra.palabra.slice(1)
+        : palabra.palabra}
     </li>
   );
 };
@@ -19,4 +21,5 @@ Palabra.propTypes = {
   anyadirPalabra: PropTypes.func,
   borrarPalabra: PropTypes.func,
   frase: PropTypes.bool.isRequired,
+  first: PropTypes.bool,
 };
