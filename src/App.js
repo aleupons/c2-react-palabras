34936a42cf390ebb5691b2690509c6752dae9561
aleupons <apahoquei@gmail.com>
2021-06-18 13:07:58 +0200

    Copiar maqueta a React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="palabras">
+        <ul className="lista-palabras">
+          <li>me</li>
+          <li data-lenguaje="si">JavaScript</li>
+          <li>el</li>
+          <li>la</li>
+          <li>gusta</li>
+          <li>odio</li>
+          <li>no</li>
+          <li>muy</li>
+          <li>y</li>
+          <li data-lenguaje="si">Java</li>
+          <li>mucho</li>
+          <li>de</li>
+          <li>poco</li>
+          <li>programador</li>
+          <li>amigo</li>
+          <li>programadora</li>
+          <li>amiga</li>
+          <li>soy</li>
+          <li>también</li>
+          <li>pero</li>
+          <li>gustan</li>
+          <li>gatos</li>
+          <li>los</li>
+          <li>perros</li>
+        </ul>
+        <ul className="resultado"></ul>
+      </section>
+      <section className="crear-palabras">
+        <form className="form-palabras">
+          <div className="form-grupo">
+            <input type="text" placeholder="Nueva palabra" required />
+          </div>
+          <div className="form-grupo">
+            <select required>
+              <option value="">Máximo de veces</option>
+              <option value="0">Sin límite</option>
+              <option value="1">1 vez</option>
+              <option value="2">2 veces</option>
+              <option value="3">3 veces</option>
+            </select>
+          </div>
+          <div className="form-grupo">
+            <label>
+              <input type="checkbox" /> Es un lenguaje de programación
+            </label>
+          </div>
+          <div className="form-grupo">
+            <button type="submit" disabled>
+              Crear
+            </button>
+          </div>
+        </form>
+      </section>
+      <section className="info">
+        <ul>
+          <li>
+            Nº de palabras <span>15</span>
+          </li>
+          <li>
+            Nº de caracteres <span>74</span>
+          </li>
+          <li>
+            Longitud media <span>5.32</span>
+          </li>
+          <li>
+            Contiene 2 lenguaje/s de programación
+            <ul>
+              <li>JavaScript</li>
+              <li>Java</li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+    </>
   );
 }
 
