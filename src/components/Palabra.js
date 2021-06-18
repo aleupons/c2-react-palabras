1f188eff387b3afc5a06ead94dc9fb80abe04d9a
aleupons <apahoquei@gmail.com>
2021-06-18 17:37:51 +0200

    PropTypes

--- src/components/Palabra.js
+++ src/components/Palabra.js
@@ -1,4 +1,11 @@
+import PropTypes from "prop-types";
+
 export const Palabra = (props) => {
   const { palabra, dataLenguaje } = props;
   return <li>{palabra}</li>;
+};
+
+Palabra.propTypes = {
+  palabra: PropTypes.string.isRequired,
+  dataLenguaje: PropTypes.bool.isRequired,
 };
